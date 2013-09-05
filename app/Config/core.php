a9165dad7b4068db0d27f38544f130b6b02126ca
caiknife <caiknife@foxmail.com>
2013-09-05 21:43:52 +0800

    2013年 09月 05日 星期四 21:43:52 CST

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'qweiuaslkdjfkasduqweoiflaksjdqwe');
+	Configure::write('Security.salt', 'qqqqqqqqqqqqqqqqqqqqqqqqqq');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '12983049172398946812734890989');
+	Configure::write('Security.cipherSeed', '0000000000000000');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
