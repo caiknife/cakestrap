0e3c232a26923ac72d437858031d8b98ae701ea0
caiknife <caiknife@foxmail.com>
2013-09-08 03:40:53 +0800

    完善event代码示例

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -13,23 +13,47 @@ class EventsController extends AppController {
     public function beforeFilter() {
         parent::beforeFilter();
         
-        $this->getEventManager()->attach(function($profile){
-            pr('In an anonymous function.');
-            pr($profile);
-            // return 'anonymous function';
-        }, 'Controller.events.index', array(
-            'passParams' => true,
-            'priority' => 2,
-        ));
+        $this->getEventManager()->attach(
+            function($profile){
+                pr('In an anonymous function with passParams set to true.');
+                pr($profile);
+            
+            }, 'Controller.events.index', array(
+                'passParams' => true,
+                'priority' => 7,
+            )
+        );
 
-        $this->getEventManager()->attach('print_profile', 
+        $this->getEventManager()->attach(
+            function($event){
+                pr('In an anonymous with passParams set to true to false.');
+                pr($event->data['profile']);
+            }, 
+            'Controller.events.index', 
+            array(
+                'priority' => 5,
+            )
+        );
+
+        $this->getEventManager()->attach(
+            'print_profile', 
             'Controller.events.index', 
             array(
                 'passParams' => true,
                 'priority' => 1,
-            ));
+            )
+        );
 
-        $this->getEventManager()->attach(array($this, '_profile'), 
+        $this->getEventManager()->attach(
+            'print_profile_with_event', 
+            'Controller.events.index', 
+            array(
+                'priority' => 3,
+            )
+        );
+
+        $this->getEventManager()->attach(
+            array($this, '_profile'), 
             'Controller.events.index',
             array('priority' => -1)
         );
@@ -41,12 +65,26 @@ class EventsController extends AppController {
      * @return [type] [description]
      */
     public function index() {
-        $event = new CakeEvent('Controller.events.index', 
+        /**
+         * CakeEvent构造函数接受三个参数
+         * @param $name 事件名称，尽量全局唯一
+         * @param $subject 被观察者对象，一般都用 $this
+         * @param $data 保存参数，可以通过 $event->data 获得
+         * @type {CakeEvent}
+         */
+        $event = new CakeEvent(
+            'Controller.events.index', 
             $this, 
             array(
-                'profile' => func_get_args(),
+                'profile' => array(
+                    'name' => 'caiknife',
+                    'email' => 'caiknife@foxmail.com',
+                ),
             )
         );
+        /**
+         * 进行广播，调用观察者的方法
+         */
         $this->getEventManager()->dispatch($event);
 
         debug($event->result);
@@ -60,21 +98,32 @@ class EventsController extends AppController {
 }
 
 function print_profile($profile) {
-    pr('In an pre-defined function.');
+    pr('In an pre-defined function with passParams set to true.');
     pr($profile);
-    // return 'pre-defined function';
+}
+
+function print_profile_with_event($event) {
+    pr('In an pre-defined function with passParams set to false.');
+    pr($event->data);
 }
 
 class ProfileListener implements CakeEventListener {
     public function implementedEvents() {
         return array(
-            'Controller.events.index' => array('callable' => 'profile' , 'priority' => -2),
+            'Controller.events.index' => array(
+                'callable' => 'profile', 
+                'priority' => -2
+            ),
         );
     }
 
     public function profile($event) {
         pr('In an class listener.');
         pr($event->data);
+        // 被观察者是我们的 EventsController 对象的一个实例
+        pr(get_class($event->subject()));
+        // 可以像平常一样调用 Controller 的各种方法
+        pr($event->subject()->request->params);
         $event->result[__METHOD__] = 'class method';
     }
 }
